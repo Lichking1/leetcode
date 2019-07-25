23b1c343f0c918367383b6a62476e4f9d1a98d59
LichKing <827313582@qq.com>
2019-07-25 12:22:44 +0800

    update T

--- Week1/T876.java
+++ Week1/T876.java
@@ -14,6 +14,7 @@ class Solution {
         
         ListNode fast = head;
         ListNode slow = head;
+        
         while(fast !=null &&fast.next != null){           
             fast = fast.next.next;
             slow = slow.next;
